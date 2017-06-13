aee746538115114a93b95154ba56846c69d1f745
Franck Chauvel <franck.chauvel@gmail.com>
2017-06-13 14:31:43 +0200

    Catching connection exceptions properly

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -10,7 +10,7 @@
 
 
 from unittest import TestCase
-from requests import get, head
+from requests import get
 from subprocess import Popen
 from time import sleep
 
@@ -25,29 +25,39 @@ class AcceptanceTests(TestCase):
     Start the server, listening on localhost, and send HTTP requests
     and check responses.
     """
-
+    
+    MAX_ATTEMPTS = 3
+    DELAY = 5
 
     def setUp(self):
         self.log_file = open("acceptance.log", "w")
         self.arcadia_mocks = Popen(["arcadiamock"], 
                                    stdout=self.log_file, 
                                    stderr=self.log_file)
-        self._wait_for_arcadia_mocks()
-        
-    def _wait_for_arcadia_mocks(self):
-        attempts = 3
-        while attempts > 0:
-            if head(Pages.ABOUT).status_code == 200: return
-            attempts -= 1
-            sleep(1)
-        message = "Cannot start arcadiamock! Can't access '{page}'!".format(page=Pages.ABOUT)
-        raise ValueError(message)
 
     def tearDown(self):
         self.log_file.close()
         self.arcadia_mocks.kill()
 
     def test_about(self):
-        response = get(Pages.ABOUT)
+        response = self._fetch(Pages.ABOUT)
         self.assertEqual(200, response.status_code)
+
+    def _fetch(self, page):
+        attempt = self.MAX_ATTEMPTS
+        while attempt >= 0:
+            try:
+                response = get(page)
+                return response
+            except:
+                sleep(self.DELAY)
+                attempt -= 1
+        message = "Cannot access '{page}' after {attempts} attempts.".format(
+            page=page,
+            attempts=self.MAX_ATTEMPTS)
+        raise RuntimeError(message)
+                
+
+
+
         
