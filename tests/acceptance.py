7bfa84461624b22198fa67a5c2067f97fa92c7e3
Franck Chauvel <franck.chauvel@gmail.com>
2017-06-15 18:35:28 +0200

    Covering subprocess on windows

--- tests/acceptance.py
+++ tests/acceptance.py
@@ -14,7 +14,11 @@ from requests import get
 from requests.exceptions import ConnectionError
 from subprocess import Popen
 from time import sleep
+from signal import SIGTERM
+from sys import platform
+from os import kill
 
+from arcadiamock.utils import spawn
 
 class Pages:
     BASE = "http://localhost:5000"
@@ -27,19 +31,22 @@ class AcceptanceTests(TestCase):
     and check responses.
     """
     
+    SERVER_START_COMMAND = ["arcadiamock"]
+    LOG_FILE = "acceptance.log"
+    ERROR_SERVER_IS_DOWN = "The arcadia-mock server has stopped. Check '{log}'"
+    ERROR_CANNOT_GET_PAGE = "Cannot access '{page}' after {attempts} attempts."
+
     MAX_ATTEMPTS = 3
     DELAY = 5
 
     def setUp(self):
-        self.log_file = open("acceptance.log", "w")
-        self.server = Popen(["arcadiamock"],
-                            stdout=self.log_file,
-                            stderr=self.log_file)
+        self.log_file = open(self.LOG_FILE, "w")
+        self.server = spawn(self.SERVER_START_COMMAND, self.log_file)
 
-    def tearDown(self):
+    def tearDown(self): 
         self.log_file.close()
         self.server.terminate()
-        
+
     def test_about(self):
         response = self._fetch(Pages.ABOUT)
         self.assertEqual(200, response.status_code)
@@ -48,16 +55,18 @@ class AcceptanceTests(TestCase):
         attempt = self.MAX_ATTEMPTS
         while attempt >= 0:
             try:
-                response = get(page)
-                return response
+                attempt -= 1
+                self._ensure_server_is_alive()
+                return get(page)
             except ConnectionError:
                 sleep(self.DELAY)
-                attempt -= 1
-        message = "Cannot access '{page}' after {attempts} attempts.".format(
-            page=page,
-            attempts=self.MAX_ATTEMPTS)
-        raise RuntimeError(message)
+        message = self.ERROR_CANNOT_GET_PAGE.format(page=page, attempts=self.MAX_ATTEMPTS)
+        self.fail(message)
                 
+    def _ensure_server_is_alive(self):
+        if self.server.poll() is not None:
+            message = self.ERROR_SERVER_IS_DOWN.format(log=self.LOG_FILE)
+            self.fail(message)
 
 
 
