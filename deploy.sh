5d45a7f3ca072acf8a9e84ba0502e97adcc19c18
vassik <anatoly.vasilevskiy@sintef.no>
2017-07-06 16:25:05 +0200

    added Dockefile and deploy.sh which is copy past from francks version, but it builds and runs a container from the source code in current directory

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+IMAGE=vassik/arcadia-mock:latest
+APP=arcadia-mock
+
+docker stop $APP
+docker rm $APP
+docker rmi -f $IMAGE
+docker build --no-cache=true --rm -t $IMAGE .
+docker run -d -p 80:1234 --name $APP $IMAGE
